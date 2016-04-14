787f47330d4d9ea529834ea722d25f887e84c57a
Arnaldo de Moraes Pereira <arnaldo@luizalabs.com>
2016-04-14 16:01:48 -0300

    added kubernetes install scripts and docker registry   (replication controller, persistent volume - and claim, service)

---
+++ k8s/k8s-lab-env.sh
@@ -0,0 +1,12 @@
+export KUBERNETES_PROVIDER=aws
+
+export KUBE_AWS_ZONE=us-west-1a
+export NUM_NODES=2
+
+export MASTER_SIZE=m3.medium
+export NODE_SIZE=m3.medium
+
+# https://github.com/kubernetes/kubernetes/tree/master/cluster/addons/registry#how-it-works
+export KUBE_ENABLE_CLUSTER_REGISTRY=true
+
+export ENABLE_NODE_PUBLIC_IP=false

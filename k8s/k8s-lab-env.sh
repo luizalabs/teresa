f9fdcb56d03c0f117171d6e8283960790cdec3d0
eraldobernardino-luizalabs <eraldo.bernardino@luizalabs.com>
2016-04-26 17:44:24 -0300

    Update k8s-lab-env.sh

--- k8s/k8s-lab-env.sh
+++ k8s/k8s-lab-env.sh
@@ -1,13 +1,11 @@
 export KUBERNETES_PROVIDER=aws
 export KUBE_ENABLE_NODE_AUTOSCALER=true
-
-export KUBE_AWS_ZONE=us-west-1a
-export NUM_NODES=2
-
+export KUBE_AWS_ZONE=sa-east-1a
+export NUM_NODES=5
 export MASTER_SIZE=m3.medium
-export NODE_SIZE=m3.medium
+export NODE_SIZE=m3.large
 
 # https://github.com/kubernetes/kubernetes/tree/master/cluster/addons/registry#how-it-works
 export KUBE_ENABLE_CLUSTER_REGISTRY=true
+export ENABLE_NODE_PUBLIC_IP=true
 
-export ENABLE_NODE_PUBLIC_IP=false

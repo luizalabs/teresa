506e8871674120060803e53404652434b8deb792
Eraldo S. Bernardino <eraldo.bernardino@luizalabs.com>
2016-05-10 07:30:16 -0700

    atualizacoes das topologias

--- k8s/k8s-lab-env.sh
+++ k8s/k8s-lab-env.sh
@@ -1,11 +1,9 @@
-export KUBERNETES_PROVIDER=aws
-export KUBE_ENABLE_NODE_AUTOSCALER=true
-export KUBE_AWS_ZONE=sa-east-1a
-export NUM_NODES=5
+export KUBE_AWS_ZONE=us-east-1a
+export AWS_S3_REGION=us-east-1
+export NUM_NODES=2
 export MASTER_SIZE=m3.medium
 export NODE_SIZE=m3.large
-
-# https://github.com/kubernetes/kubernetes/tree/master/cluster/addons/registry#how-it-works
+export KUBE_ENABLE_NODE_PUBLIC_IP=true
 export KUBE_ENABLE_CLUSTER_REGISTRY=true
-export ENABLE_NODE_PUBLIC_IP=true
-
+export KUBERNETES_PROVIDER=aws
+export KUBE_ENABLE_NODE_AUTOSCALER=true

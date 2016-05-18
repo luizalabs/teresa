60205a26aba0426b3b961a38d179a1cfaf16962e
Arnaldo de Moraes Pereira <arnaldo@luizalabs.com>
2016-05-18 19:43:41 -0300

    Teresa API. To access the swagger UI: `cd api/; make swagger-docs`

---
+++ api/restapi/doc.go
@@ -0,0 +1,24 @@
+/*Package restapi Teresa API
+
+The Teresa PaaS API
+
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /v1
+    Version: 0.0.1
+
+    Consumes:
+    - application/com.luizalabs.teresa.v1+json
+
+    - multipart/form-data
+
+
+    Produces:
+    - application/com.luizalabs.teresa.v1+json
+
+
+swagger:meta
+*/
+package restapi

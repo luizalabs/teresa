7232de810642bc635ee603675543103f800e5bc4
Arnaldo de Moraes Pereira <arnaldo@luizalabs.com>
2016-05-18 19:43:41 -0300

    Teresa API. To access the swagger UI: `cd api/; make swagger-docs`

---
+++ api/docs/webserver.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// receive the swagger spec path from the command line
+	if len(os.Args) != 2 {
+		log.Printf("Usage: %s swagger.yml")
+		os.Exit(1)
+	}
+	swaggerfile := os.Args[1]
+
+	// listen on a free port specified by the OS
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("listening on: %s", listener.Addr().String())
+
+	// serve the whole content of docs/ on '/'
+	fs := http.FileServer(http.Dir("docs"))
+	http.Handle("/", fs)
+
+	// swagger.yml is accessible through /swagger.yml
+	http.HandleFunc("/swagger.yml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/yaml")
+		w.WriteHeader(http.StatusOK)
+		data, err := ioutil.ReadFile(swaggerfile)
+		if err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+		fmt.Fprint(w, string(data))
+	})
+
+	url := listener.Addr().String()
+	log.Printf("Opening %s...", url)
+	cmd := exec.Command("open", fmt.Sprintf("http://%s", url))
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	panic(http.Serve(listener, nil))
+}

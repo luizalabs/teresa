b5e71dc4b6ebf0b6c27df43b37bbf04f1b5f5bc7
ederprado-luizalabs <eder.prado@luizalabs.com>
2016-05-30 17:14:03 -0300

    Rem: some useless comments

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -20,14 +20,5 @@ var versionCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(versionCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	versionCmd.Flags().Bool("only-client", false, "Show only the client info")
-
 }

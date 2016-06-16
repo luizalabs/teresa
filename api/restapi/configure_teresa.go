441f0194f729f896e9556ec017e4006f0d045b19
Arnaldo de Moraes Pereira <arnaldo@luizalabs.com>
2016-06-16 13:41:57 -0300

    moved handlers logic from `restapi/configure_teresa.go` to `handlers/`

--- api/restapi/configure_teresa.go
+++ api/restapi/configure_teresa.go
@@ -2,15 +2,13 @@ package restapi
 
 import (
 	"crypto/tls"
-	"fmt"
 	"net/http"
-
-	"golang.org/x/crypto/bcrypt"
 
 	errors "github.com/go-openapi/errors"
 	runtime "github.com/go-openapi/runtime"
 	middleware "github.com/go-openapi/runtime/middleware"
 
+	"github.com/luizalabs/paas/api/handlers"
 	"github.com/luizalabs/paas/api/restapi/operations"
 	"github.com/luizalabs/paas/api/restapi/operations/apps"
 	"github.com/luizalabs/paas/api/restapi/operations/auth"
@@ -18,9 +16,7 @@ import (
 	"github.com/luizalabs/paas/api/restapi/operations/teams"
 	"github.com/luizalabs/paas/api/restapi/operations/users"
 
-	"github.com/astaxie/beego/orm"
 	"github.com/luizalabs/paas/api/models"
-	storage "github.com/luizalabs/paas/api/models/storage"
 )
 
 // This file is safe to edit. Once it exists it will not be overwritten
@@ -74,34 +70,9 @@ func configureAPI(api *operations.TeresaAPI) http.Handler {
 
 	// create a user
 	api.UsersCreateUserHandler = users.CreateUserHandlerFunc(func(params users.CreateUserParams, principal interface{}) middleware.Responder {
-		o := orm.NewOrm()
-		o.Using("default")
-		h, err := bcrypt.GenerateFromPassword([]byte(*params.Body.Password), bcrypt.DefaultCost)
-		if err != nil {
-			return users.NewCreateUserDefault(500) // FIXME: better handling
-		}
-		hashedPassword := string(h)
-		u := models.User{
-			Name:     params.Body.Name,
-			Email:    params.Body.Email,
-			Password: &hashedPassword,
-		}
-		su := storage.User{
-			Name:     *u.Name,
-			Email:    *u.Email,
-			Password: *u.Password,
-		}
-		id, err := o.Insert(&su)
-		if err != nil {
-			fmt.Printf("UsersCreateUserHandler failed: %s\n", err)
-			return users.NewCreateUserDefault(422)
-		}
-		u.ID = id
-		u.Password = nil
-		r := users.NewCreateUserCreated()
-		r.SetPayload(&u)
-		return r
+		return handlers.CreateUserHandler(params, principal)
 	})
+
 	api.AppsGetAppDetailsHandler = apps.GetAppDetailsHandlerFunc(func(params apps.GetAppDetailsParams, principal interface{}) middleware.Responder {
 		return middleware.NotImplemented("operation apps.GetAppDetails has not yet been implemented")
 	})
@@ -123,27 +94,7 @@ func configureAPI(api *operations.TeresaAPI) http.Handler {
 
 	// get a single user from db
 	api.UsersGetUserDetailsHandler = users.GetUserDetailsHandlerFunc(func(params users.GetUserDetailsParams, principal interface{}) middleware.Responder {
-		o := orm.NewOrm()
-		o.Using("default")
-		su := storage.User{Id: params.UserID}
-		err := o.Read(&su)
-		if err == orm.ErrNoRows {
-			fmt.Println("No result found")
-			return users.NewGetUserDetailsNotFound()
-		} else if err == orm.ErrMissPK {
-			fmt.Printf("No user with ID [%s] found\n", params.UserID)
-			return users.NewGetUserDetailsNotFound()
-		} else {
-			fmt.Printf("Found user with ID [%d] name [%s] email [%s]\n", su.Id, su.Name, su.Email)
-			r := users.NewGetUserDetailsOK()
-			u := models.User{
-				ID:    su.Id,
-				Name:  &su.Name,
-				Email: &su.Email,
-			}
-			r.SetPayload(&u)
-			return r
-		}
+		return handlers.GetUserDetailsHandler(params, principal)
 	})
 
 	api.UsersGetUsersHandler = users.GetUsersHandlerFunc(func(params users.GetUsersParams, principal interface{}) middleware.Responder {

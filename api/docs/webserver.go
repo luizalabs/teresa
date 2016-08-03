f5c147d476cf7fc3050020b7f90fc3f1b930ddf8
Arnaldo de Moraes Pereira <arnaldo@luizalabs.com>
2016-08-03 16:21:09 -0300

    minor cleanup
    - minor fix and improvements on api docs' webserver
    - removed unused commands

--- api/docs/webserver.go
+++ api/docs/webserver.go
@@ -13,15 +13,14 @@ import (
 func main() {
 	// receive the swagger spec path from the command line
 	if len(os.Args) != 2 {
-		log.Printf("Usage: %s swagger.yml")
-		os.Exit(1)
+		log.Fatal("Missing swagger file as argument")
 	}
 	swaggerfile := os.Args[1]
 
 	// listen on a free port specified by the OS
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
-		panic(err)
+		log.Fatal(err.Error())
 	}
 	log.Printf("listening on: %s", listener.Addr().String())
 
@@ -35,7 +34,7 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		data, err := ioutil.ReadFile(swaggerfile)
 		if err != nil {
-			panic(err)
+			log.Fatal(err.Error())
 		}
 		w.Header().Set("Content-Length", fmt.Sprint(len(data)))
 		fmt.Fprint(w, string(data))
@@ -45,7 +44,7 @@ func main() {
 	log.Printf("Opening %s...", url)
 	cmd := exec.Command("open", fmt.Sprintf("http://%s", url))
 	if err := cmd.Start(); err != nil {
-		panic(err)
+		log.Fatal(err.Error())
 	}
 
 	panic(http.Serve(listener, nil))

eac525a1cf0b16acee683a317dde40c876a3acb5
ederprado-luizalabs <eder.prado@luizalabs.com>
2016-08-09 10:43:56 -0300

    add vendor to package

---
+++ vendor/github.com/aws/aws-sdk-go/aws/version.go
@@ -0,0 +1,8 @@
+// Package aws provides core functionality for making requests to AWS services.
+package aws
+
+// SDKName is the name of this AWS SDK
+const SDKName = "aws-sdk-go"
+
+// SDKVersion is the version of this SDK
+const SDKVersion = "release-v0.10.0"

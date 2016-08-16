6cbc1a039bac56be3bb984f3857b76c6af406712
ederprado-luizalabs <eder.prado@luizalabs.com>
2016-08-16 18:18:26 -0300

    get teams now returning all teams when user is admin + isMember flag

--- models/team.go
+++ models/team.go
@@ -25,9 +25,17 @@ type Team struct {
 	 */
 	Email strfmt.Email `json:"email,omitempty"`
 
+	/* i am member
+	 */
+	IAmMember bool `json:"iAmMember,omitempty"`
+
 	/* id
 	 */
 	ID int64 `json:"id,omitempty"`
+
+	/* members
+	 */
+	Members []*User `json:"members,omitempty"`
 
 	/* name
 
@@ -45,6 +53,11 @@ func (m *Team) Validate(formats strfmt.Registry) error {
 	var res []error
 
 	if err := m.validateApps(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if err := m.validateMembers(formats); err != nil {
 		// prop
 		res = append(res, err)
 	}
@@ -75,6 +88,30 @@ func (m *Team) validateApps(formats strfmt.Registry) error {
 		if m.Apps[i] != nil {
 
 			if err := m.Apps[i].Validate(formats); err != nil {
+				return err
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (m *Team) validateMembers(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Members) { // not required
+		return nil
+	}
+
+	for i := 0; i < len(m.Members); i++ {
+
+		if swag.IsZero(m.Members[i]) { // not required
+			continue
+		}
+
+		if m.Members[i] != nil {
+
+			if err := m.Members[i].Validate(formats); err != nil {
 				return err
 			}
 		}

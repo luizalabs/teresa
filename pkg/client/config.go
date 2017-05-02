9afc65bb3ed046fde01196250b4ccb066fdf1bb6
Diego Garcia <drgarcia1986@gmail.com>
2017-05-02 18:07:53 -0300

    add pkg `client/config` to deal with teresa config file

---
+++ pkg/client/config.go
@@ -0,0 +1,101 @@
+package client
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type ClusterConfig struct {
+	Server string `yaml:"server"`
+	Token  string `yaml:"token"`
+}
+
+type Config struct {
+	Clusters       map[string]ClusterConfig `yaml:"clusters"`
+	CurrentCluster string                   `yaml:"current_cluster"`
+}
+
+var (
+	DefaultConfigFileLocation string
+	ErrInvalidConfigFile      = errors.New("Invalid config file")
+	ErrInvalidDir             = errors.New("Invalid directory")
+)
+
+func init() {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		// print a warning ?
+		return
+	}
+	DefaultConfigFileLocation = filepath.Join(homeDir, ".teresa", "config.yaml")
+}
+
+func SaveToken(cfgFile, token string) error {
+	cfg, err := readConfigFile(cfgFile)
+	if err != nil {
+		return err
+	}
+	cc, ok := cfg.Clusters[cfg.CurrentCluster]
+	if !ok {
+		return ErrInvalidConfigFile
+	}
+
+	cc.Token = token
+	cfg.Clusters[cfg.CurrentCluster] = cc
+	return SaveConfigFile(cfgFile, cfg)
+}
+
+func SaveConfigFile(path string, cfg *Config) error {
+	b, err := yaml.Marshal(&cfg)
+	if err != nil {
+		return err
+	}
+	dir := filepath.Dir(path)
+	stat, err := os.Stat(dir)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+		if err = os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+	} else if !stat.IsDir() {
+		return ErrInvalidDir
+	}
+
+	return ioutil.WriteFile(path, b, 0600)
+}
+
+func GetConfig(cfgFile string) (*ClusterConfig, error) {
+	cfg, err := readConfigFile(cfgFile)
+	if err != nil {
+		return nil, err
+	}
+	currentClusterConfig, ok := cfg.Clusters[cfg.CurrentCluster]
+	if !ok {
+		return nil, ErrInvalidConfigFile
+	}
+	return &currentClusterConfig, nil
+}
+
+func readConfigFile(cfgFile string) (*Config, error) {
+	y, err := ioutil.ReadFile(cfgFile)
+	if err != nil {
+		return nil, ErrInvalidConfigFile
+	}
+
+	c := new(Config)
+	if err = yaml.Unmarshal(y, c); err != nil {
+		return nil, ErrInvalidConfigFile
+	}
+	if c.Clusters == nil {
+		c.Clusters = make(map[string]ClusterConfig)
+	}
+	return c, nil
+}

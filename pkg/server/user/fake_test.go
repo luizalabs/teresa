63b4d1f1d295851b86389dcd733840d2446b6195
Diego Garcia <drgarcia1986@gmail.com>
2017-05-02 16:14:42 -0300

    add pkg `server/user` to deal with users domain

---
+++ pkg/server/user/fake_test.go
@@ -0,0 +1,32 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/luizalabs/teresa-api/pkg/server/auth"
+)
+
+func TestFakeOperationsLogin(t *testing.T) {
+	fake := NewFakeOperations()
+
+	expectedEmail := "teresa@luizalabs.com"
+	expectedPassword := "123456"
+	fake.(*FakeOperations).Storage[expectedEmail] = expectedPassword
+
+	token, err := fake.Login(expectedEmail, expectedPassword)
+	if err != nil {
+		t.Fatal("Error on perform Login in FakeUserOperations: ", err)
+	}
+	expectedToken := "good token"
+	if token != expectedToken {
+		t.Errorf("expected %s, got %s", expectedToken, token)
+	}
+}
+
+func TestFakeOperationsBadLogin(t *testing.T) {
+	fake := NewFakeOperations()
+
+	if _, err := fake.Login("invalid@luizalabs.com", "foo"); err != auth.ErrPermissionDenied {
+		t.Errorf("expected ErrPermissionDenied, got %s", err)
+	}
+}

09df47a7fe02c0b5966c3abb4415303fd9c3359e
Diego Garcia <drgarcia1986@gmail.com>
2017-05-02 14:51:09 -0300

    add pkg `secrets` to deal with secrets (like rsa keys)

---
+++ pkg/server/secrets/fake.go
@@ -0,0 +1,27 @@
+package secrets
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+type Fake struct {
+	private *rsa.PrivateKey
+	public  *rsa.PublicKey
+}
+
+func (f *Fake) PrivateKey() (*rsa.PrivateKey, error) {
+	return f.private, nil
+}
+
+func (f *Fake) PublicKey() (*rsa.PublicKey, error) {
+	return f.public, nil
+}
+
+func NewFake() (Secrets, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
+	return &Fake{private: key, public: &key.PublicKey}, nil
+}

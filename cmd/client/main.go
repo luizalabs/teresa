7c0285a4faafef2e15103d20276e2bc03c541191
Alex Guerra <alex.guerra@luizalabs.com>
2017-05-12 14:35:06 -0300

    Disable client side grpc log

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/luizalabs/teresa-api/cmd/client/cmd"
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/luizalabs/teresa-api/cmd/client/cmd"
+
+	"google.golang.org/grpc/grpclog"
+)
+
+func init() {
+	grpclog.SetLogger(log.New(ioutil.Discard, "", 0))
+}
 
 func main() {
 	cmd.Execute()

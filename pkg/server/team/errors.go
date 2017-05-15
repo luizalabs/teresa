0ae8309ba9eaa271d841fd8e4c2e68d6d176033d
Diego Garcia <drgarcia1986@gmail.com>
2017-05-15 16:43:25 -0300

    add pkg `server/team` to deal with team domain

---
+++ pkg/server/team/errors.go
@@ -0,0 +1,5 @@
+package team
+
+import "errors"
+
+var ErrTeamAlreadyExists = errors.New("Team already exists")
---
+++ pkg/server/team/team.go
@@ -0,0 +1,31 @@
+package team
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/luizalabs/teresa-api/models/storage"
+)
+
+type Operations interface {
+	Create(name, email, url string) error
+}
+
+type DatabaseOperations struct {
+	DB *gorm.DB
+}
+
+func (dbt *DatabaseOperations) Create(name, email, url string) error {
+	t := new(storage.Team)
+	if !dbt.DB.Where(&storage.Team{Name: name}).First(t).RecordNotFound() {
+		return ErrTeamAlreadyExists
+	}
+
+	t.Name = name
+	t.Email = email
+	t.URL = url
+	return dbt.DB.Save(t).Error
+}
+
+func NewDatabaseOperations(db *gorm.DB) Operations {
+	db.AutoMigrate(&storage.Team{})
+	return &DatabaseOperations{DB: db}
+}

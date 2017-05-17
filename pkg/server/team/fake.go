9312fbfea1f36af42bde50cf6571cadda28f9bc4
Diego Garcia <drgarcia1986@gmail.com>
2017-05-17 17:08:01 -0300

    create method `AddUser` on Team Operations interface and create a fake implementation

--- pkg/server/team/fake.go
+++ pkg/server/team/fake.go
@@ -4,11 +4,14 @@ import (
 	"sync"
 
 	"github.com/luizalabs/teresa-api/models/storage"
+	"github.com/luizalabs/teresa-api/pkg/server/user"
 )
 
 type FakeOperations struct {
 	mutex   *sync.RWMutex
 	Storage map[string]*storage.Team
+
+	UserOps user.Operations
 }
 
 func (f *FakeOperations) Create(name, email, url string) error {
@@ -23,8 +26,33 @@ func (f *FakeOperations) Create(name, email, url string) error {
 	return nil
 }
 
+func (f *FakeOperations) AddUser(name, email string) error {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	t, found := f.Storage[name]
+	if !found {
+		return ErrNotFound
+	}
+
+	u, err := f.UserOps.GetUser(email)
+	if err != nil {
+		return err
+	}
+
+	for _, userOfTeam := range t.Users {
+		if userOfTeam.Email == email {
+			return ErrUserAlreadyInTeam
+		}
+	}
+
+	t.Users = append(t.Users, *u)
+	return nil
+}
+
 func NewFakeOperations() Operations {
 	return &FakeOperations{
 		mutex:   &sync.RWMutex{},
-		Storage: make(map[string]*storage.Team)}
+		Storage: make(map[string]*storage.Team),
+		UserOps: user.NewFakeOperations()}
 }

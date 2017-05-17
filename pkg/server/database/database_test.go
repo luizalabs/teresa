7b6199d0de4bea36337f5a39a6cdfa6527bc1e59
Diego Garcia <drgarcia1986@gmail.com>
2017-05-17 17:53:05 -0300

    add simple test in database connection factory

---
+++ pkg/server/database/database_test.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func TestNew(t *testing.T) {
+	db, err := New(Config{Database: ":memory:"})
+	if err != nil {
+		t.Fatal("error trying to create a new database connection:", err)
+	}
+	if db == nil {
+		t.Fatal("expected a instance of gorm.DB, got nil")
+	}
+	if err = db.DB().Ping(); err != nil {
+		t.Error("Error on make a ping to in memory database:", err)
+	}
+}

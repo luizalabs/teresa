0edb24d9c128b864326a41108d6d358ac2123ee5
Diego Garcia <drgarcia1986@gmail.com>
2017-05-22 11:43:55 -0300

    standarlize users errors to use grpc status instead of errors.New

--- pkg/server/user/errors.go
+++ pkg/server/user/errors.go
@@ -1,14 +1,12 @@
 package user
 
 import (
-	"errors"
-
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 var (
-	ErrNotFound          = errors.New("User not found")
+	ErrNotFound          = status.Errorf(codes.NotFound, "User not found")
 	ErrUserAlreadyExists = status.Errorf(codes.AlreadyExists, "User already exists")
 	ErrInvalidPassword   = status.Errorf(codes.InvalidArgument, "Invalid password")
 )

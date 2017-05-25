35700f1c9434938569e221328c063d3ade5b6c7d
Diego Garcia <drgarcia1986@gmail.com>
2017-05-25 14:17:11 -0300

    add pkg `validations` with e-mail validation

---
+++ pkg/server/validations/validations_test.go
@@ -0,0 +1,27 @@
+package validations
+
+import "testing"
+
+func TestValidateEmail(t *testing.T) {
+	var testCases = []struct {
+		email          string
+		expectedResult bool
+	}{
+		{"gopher@luizalabs.com", true},
+		{"gopher@magazineluiza.com.br", true},
+		{"gopher123@luizalabs.com", true},
+		{"gopher.vimmer@luizalabs.com", true},
+		{"gopher_vimmer@luizalabs.com", true},
+		{"gopher-vimmer@luizalabs.com", true},
+		{"gopher", false},
+		{"gopher.com", false},
+		{"gopher@@luizalabs.com", false},
+		{"gopher@.com", false},
+	}
+
+	for _, tc := range testCases {
+		if b := ValidateEmail(tc.email); b != tc.expectedResult {
+			t.Errorf("expected %v, got %v (email: %s)", tc.expectedResult, b, tc.email)
+		}
+	}
+}

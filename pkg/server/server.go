9eb9347ab1c2626da86357d831ce261dfe3bb6d1
Diego Garcia <drgarcia1986@gmail.com>
2017-06-05 19:08:28 -0300

    add StreamInterceptor to deal with authentication on gRPC server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -35,6 +35,33 @@ type Server struct {
 	grpcServer *grpc.Server
 }
 
+type serverStreamWrapper struct {
+	grpc.ServerStream
+	ctx context.Context
+}
+
+func (w *serverStreamWrapper) Context() context.Context {
+	return w.ctx
+}
+
+func streamInterceptor(a auth.Auth, uOps user.Operations) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		if strings.HasSuffix(info.FullMethod, "Login") {
+			return handler(srv, stream)
+		}
+
+		ctx := stream.Context()
+		user, err := authorize(ctx, a, uOps)
+		if err != nil {
+			return err
+		}
+
+		ctx = context.WithValue(ctx, "user", user)
+		wrap := &serverStreamWrapper{stream, ctx}
+		return handler(srv, wrap)
+	}
+}
+
 func unaryInterceptor(a auth.Auth, uOps user.Operations) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		if strings.HasSuffix(info.FullMethod, "Login") {
@@ -82,6 +109,7 @@ func New(opt Options) (*Server, error) {
 	s := grpc.NewServer(
 		grpc.Creds(creds),
 		grpc.UnaryInterceptor(unaryInterceptor(opt.Auth, uOps)),
+		grpc.StreamInterceptor(streamInterceptor(opt.Auth, uOps)),
 	)
 
 	us := user.NewService(uOps)

a3d06e34a776ea4bd4e919f6ef8fff283cd73e11
Alex Guerra <alex.guerra@luizalabs.com>
2017-06-07 17:55:00 -0300

    Implement high level app creation logic

--- pkg/server/app/app_test.go
+++ pkg/server/app/app_test.go
@@ -3,6 +3,7 @@ package app
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"io"
 	"io/ioutil"
 	"strings"
@@ -18,9 +19,23 @@ import (
 
 type fakeK8sOperations struct{}
 
-type errK8sOperations struct{ Err error }
+type errK8sOperations struct {
+	Err          error
+	NamespaceErr error
+	QuotaErr     error
+	SecretErr    error
+	AutoScaleErr error
+}
 
-func (*fakeK8sOperations) Create(app *App, st st.Storage) error {
+func (*fakeK8sOperations) CreateNamespace(app *App, user string) error {
+	return nil
+}
+
+func (*fakeK8sOperations) CreateQuota(app *App) error {
+	return nil
+}
+
+func (*fakeK8sOperations) CreateSecret(appName, secretName string, data map[string][]byte) error {
 	return nil
 }
 
@@ -45,8 +60,24 @@ func (*fakeK8sOperations) NamespaceLabel(namespace, label string) (string, error
 	return "luizalabs", nil
 }
 
-func (e *errK8sOperations) Create(app *App, st st.Storage) error {
-	return e.Err
+func (*fakeK8sOperations) CreateAutoScale(app *App) error {
+	return nil
+}
+
+func (e *errK8sOperations) CreateNamespace(app *App, user string) error {
+	return e.NamespaceErr
+}
+
+func (e *errK8sOperations) CreateQuota(app *App) error {
+	return e.QuotaErr
+}
+
+func (e *errK8sOperations) CreateSecret(appName, secretName string, data map[string][]byte) error {
+	return e.SecretErr
+}
+
+func (e *errK8sOperations) CreateAutoScale(app *App) error {
+	return e.AutoScaleErr
 }
 
 func (e *errK8sOperations) PodList(namespace string) ([]*Pod, error) {
@@ -67,7 +98,8 @@ func (e *errK8sOperations) NamespaceLabel(namespace, label string) (string, erro
 
 func TestAppOperationsCreate(t *testing.T) {
 	tops := team.NewFakeOperations()
-	ops := NewOperations(tops, &fakeK8sOperations{}, nil)
+	fakeSt := st.NewFake()
+	ops := NewOperations(tops, &fakeK8sOperations{}, fakeSt)
 	name := "luizalabs"
 	user := &storage.User{Email: "teresa@luizalabs.com"}
 	app := &App{Name: "teresa", Team: name}
@@ -83,7 +115,8 @@ func TestAppOperationsCreate(t *testing.T) {
 
 func TestAppOperationsCreateErrPermissionDenied(t *testing.T) {
 	tops := team.NewFakeOperations()
-	ops := NewOperations(tops, &fakeK8sOperations{}, nil)
+	fakeSt := st.NewFake()
+	ops := NewOperations(tops, &fakeK8sOperations{}, fakeSt)
 	name := "luizalabs"
 	user := &storage.User{Email: "teresa@luizalabs.com"}
 	app := &App{Name: "teresa", Team: name}
@@ -95,7 +128,8 @@ func TestAppOperationsCreateErrPermissionDenied(t *testing.T) {
 
 func TestAppOperationsCreateErrAppAlreadyExists(t *testing.T) {
 	tops := team.NewFakeOperations()
-	ops := NewOperations(tops, &fakeK8sOperations{}, nil)
+	fakeSt := st.NewFake()
+	ops := NewOperations(tops, &fakeK8sOperations{}, fakeSt)
 	name := "luizalabs"
 	user := &storage.User{Email: "teresa@luizalabs.com"}
 	app := &App{Name: "teresa", Team: name}
@@ -103,12 +137,8 @@ func TestAppOperationsCreateErrAppAlreadyExists(t *testing.T) {
 		Name:  name,
 		Users: []storage.User{*user},
 	}
+	ops.(*AppOperations).kops = &errK8sOperations{NamespaceErr: ErrAlreadyExists}
 
-	if err := ops.Create(user, app); err != nil {
-		t.Fatal("error creating app: ", err)
-	}
-
-	ops.(*AppOperations).kops = &errK8sOperations{Err: ErrAlreadyExists}
 	if err := ops.Create(user, app); err != ErrAlreadyExists {
 		t.Errorf("expected ErrAppAlreadyExists got %s", err)
 	}
@@ -166,5 +196,59 @@ func TestAppOperationsLogsErrNotFound(t *testing.T) {
 
 	if _, err := ops.Logs(user, "teresa", 10, false); err != ErrNotFound {
 		t.Errorf("expected ErrNotFound, got %v", err)
+	}
+}
+
+func TestAppOperationsCreateErrQuota(t *testing.T) {
+	tops := team.NewFakeOperations()
+	fakeSt := st.NewFake()
+	ops := NewOperations(tops, &fakeK8sOperations{}, fakeSt)
+	name := "luizalabs"
+	user := &storage.User{Email: "teresa@luizalabs.com"}
+	app := &App{Name: "teresa", Team: name}
+	tops.(*team.FakeOperations).Storage[name] = &storage.Team{
+		Name:  name,
+		Users: []storage.User{*user},
+	}
+	ops.(*AppOperations).kops = &errK8sOperations{QuotaErr: errors.New("Quota Error")}
+
+	if ops.Create(user, app) == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestAppOperationsCreateErrSecret(t *testing.T) {
+	tops := team.NewFakeOperations()
+	fakeSt := st.NewFake()
+	ops := NewOperations(tops, &fakeK8sOperations{}, fakeSt)
+	name := "luizalabs"
+	user := &storage.User{Email: "teresa@luizalabs.com"}
+	app := &App{Name: "teresa", Team: name}
+	tops.(*team.FakeOperations).Storage[name] = &storage.Team{
+		Name:  name,
+		Users: []storage.User{*user},
+	}
+	ops.(*AppOperations).kops = &errK8sOperations{SecretErr: errors.New("Secret Error")}
+
+	if ops.Create(user, app) == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestAppOperationsCreateErrAutoScale(t *testing.T) {
+	tops := team.NewFakeOperations()
+	fakeSt := st.NewFake()
+	ops := NewOperations(tops, &fakeK8sOperations{}, fakeSt)
+	name := "luizalabs"
+	user := &storage.User{Email: "teresa@luizalabs.com"}
+	app := &App{Name: "teresa", Team: name}
+	tops.(*team.FakeOperations).Storage[name] = &storage.Team{
+		Name:  name,
+		Users: []storage.User{*user},
+	}
+	ops.(*AppOperations).kops = &errK8sOperations{AutoScaleErr: errors.New("AutoScale Error")}
+
+	if ops.Create(user, app) == nil {
+		t.Errorf("expected error, got nil")
 	}
 }

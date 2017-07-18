292277f7af21d61e99c9cdc081829d7c89e9e384
Alex Guerra <alex.guerra@luizalabs.com>
2017-07-18 15:50:55 -0300

    Remove old server (go-swagger based) code

--- scripts/create_dummy_user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# password: teresa123
-echo 'insert into users (created_at, updated_at, name, email, password, is_admin) values ("2016-09-01 13:00:00-03:00", "2016-09-01 13:00:00-03:00", "admin", "admin@teresa.com", "$2a$10$RcJki6f/Bt.fbbtAS6ddh.05BzilrxRjWvd8ZJqpo9ToPbpiFY5.e", 1);' | sqlite3 teresa.sqlite

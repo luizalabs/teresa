47aa16bad0960415c8fc698696718dc7a35b8c26
Diego Garcia <drgarcia1986@gmail.com>
2017-07-19 16:14:35 -0300

    standardize errors from pkg user

--- pkg/server/user/user.go
+++ pkg/server/user/user.go
@@ -1,12 +1,15 @@
 package user
 
 import (
-	log "github.com/Sirupsen/logrus"
+	"fmt"
+
+	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/jinzhu/gorm"
 	"github.com/luizalabs/teresa-api/models/storage"
 	"github.com/luizalabs/teresa-api/pkg/server/auth"
+	"github.com/luizalabs/teresa-api/pkg/server/teresa_errors"
 	"github.com/luizalabs/teresa-api/pkg/server/validations"
 )
 
@@ -30,19 +33,21 @@ type DatabaseOperations struct {
 func (dbu *DatabaseOperations) Login(email, password string) (string, error) {
 	u, err := dbu.GetUser(email)
 	if err != nil {
-		log.WithField("user", email).Info("Not Found")
 		return "", auth.ErrPermissionDenied
 	}
 	if err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
-		log.Printf("Authentication failed for user [%s]\n", email)
-		return "", auth.ErrPermissionDenied
+		return "", teresa_errors.New(
+			auth.ErrPermissionDenied,
+			errors.Wrap(err, fmt.Sprintf("Authentication faield for user %s", email)),
+		)
 	}
-	log.Printf("Authentication succeeded for user [%s]\n", email)
 
 	token, err := dbu.auth.GenerateToken(email)
 	if err != nil {
-		log.WithError(err).Error("Failed to sign JWT token")
-		return "", auth.ErrPermissionDenied
+		return "", teresa_errors.New(
+			auth.ErrPermissionDenied,
+			errors.Wrap(err, "Signing JWT token"),
+		)
 	}
 	return token, nil
 }
@@ -62,10 +67,19 @@ func (dbu *DatabaseOperations) SetPassword(email, newPassword string) error {
 	}
 	pass, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return teresa_errors.New(
+			teresa_errors.ErrInternalServerError,
+			errors.Wrap(err, fmt.Sprintf("Generating the password hash to user %s", email)),
+		)
 	}
 	u.Password = string(pass)
-	return dbu.DB.Save(u).Error
+	if err = dbu.DB.Save(u).Error; err != nil {
+		return teresa_errors.New(
+			teresa_errors.ErrInternalServerError,
+			errors.Wrap(err, fmt.Sprintf("Updating password of user %s", email)),
+		)
+	}
+	return nil
 }
 
 func (dbu *DatabaseOperations) Delete(email string) error {
@@ -73,7 +87,13 @@ func (dbu *DatabaseOperations) Delete(email string) error {
 	if err != nil {
 		return err
 	}
-	return dbu.DB.Delete(u).Error
+	if err = dbu.DB.Delete(u).Error; err != nil {
+		return teresa_errors.New(
+			teresa_errors.ErrInternalServerError,
+			errors.Wrap(err, fmt.Sprintf("Deleting user %s", email)),
+		)
+	}
+	return nil
 }
 
 func (dbu *DatabaseOperations) Create(name, email, pass string, admin bool) error {
@@ -90,13 +110,22 @@ func (dbu *DatabaseOperations) Create(name, email, pass string, admin bool) erro
 	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return teresa_errors.New(
+			teresa_errors.ErrInternalServerError,
+			errors.Wrap(err, fmt.Sprintf("Generating the password hash to user %s", email)),
+		)
 	}
 	u.Name = name
 	u.Email = email
 	u.Password = string(hash)
 	u.IsAdmin = admin
-	return dbu.DB.Save(u).Error
+	if err = dbu.DB.Save(u).Error; err != nil {
+		return teresa_errors.New(
+			teresa_errors.ErrInternalServerError,
+			errors.Wrap(err, fmt.Sprintf("Creating user %s", email)),
+		)
+	}
+	return nil
 }
 
 func NewDatabaseOperations(db *gorm.DB, a auth.Auth) Operations {

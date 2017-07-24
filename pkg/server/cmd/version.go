b30322836d1a9a2bd280a99d049dc868fb091f1a
Alex Guerra <alex.guerra@luizalabs.com>
2017-07-24 16:27:53 -0300

    Add server version command

---
+++ pkg/server/cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/luizalabs/teresa-api/pkg/version"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the server version information",
+	Run:   showVersion,
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+func showVersion(cmd *cobra.Command, args []string) {
+	fmt.Printf("Version: %s\n", version.Version)
+}

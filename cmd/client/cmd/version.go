ef0d607e650915ababdcd8cd8affc8e061ea161a
Alex Guerra <alex.guerra@luizalabs.com>
2017-07-31 16:36:17 -0300

    Unify server and client versions

--- cmd/client/cmd/version.go
+++ cmd/client/cmd/version.go
@@ -3,20 +3,18 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/luizalabs/teresa-api/pkg/version"
 	"github.com/spf13/cobra"
 )
 
-// returns the version of the cli and server
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Shows the client and selected server version",
+	Short: "Shows the client version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Client version: %s\n", version)
+		fmt.Printf("Version: %s\n", version.Version)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
-	// TODO: will be necessary in the future when we will return more info
-	// versionCmd.Flags().Bool("only-client", false, "Show only the client info")
 }

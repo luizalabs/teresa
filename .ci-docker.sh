87fbba677270585679b342eaba475bb72b17a78b
Diego Garcia <drgarcia1986@gmail.com>
2017-08-15 15:46:28 -0300

    create another build stage to generate docker image

--- .ci-docker.sh
+++ .ci-docker.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+if [ -z "$TRAVIS_TAG" ]; then
+    echo "skip docker imagem generation (no tag detected)"
+    exit 0
+fi
+
 echo "Generate and upload the latest TERESA-SERVER image to Docker HUB"
-docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 docker build -t "$DOCKER_REGISTRY"/"$DOCKER_IMAGE":"$TRAVIS_TAG" .
 docker push "$DOCKER_REGISTRY"/"$DOCKER_IMAGE":"$TRAVIS_TAG"
 
 exit 0;
+

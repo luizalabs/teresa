93f76892e1c728ac200767c643a1861be7fe49bc
Diego Garcia <drgarcia1986@gmail.com>
2017-08-28 15:11:45 -0300

    revert .ci-docker.sh script

--- .ci-docker.sh
+++ .ci-docker.sh
@@ -8,7 +8,9 @@ if [ -z "$TRAVIS_TAG" ]; then
 fi
 
 echo "Generate and upload the latest TERESA-SERVER image to Docker HUB"
-make docker-login docker-build docker-push
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+docker build -t "$DOCKER_REGISTRY"/"$DOCKER_IMAGE":"$TRAVIS_TAG" .
+docker push "$DOCKER_REGISTRY"/"$DOCKER_IMAGE":"$TRAVIS_TAG"
 
 exit 0;
 

7de5c4b90673c74279998fef091ff5c4d5f7de74
Alex Guerra <alex.guerra@luizalabs.com>
2017-08-29 11:02:05 -0300

    Add fake deploy operations

---
+++ pkg/server/deploy/fake.go
@@ -0,0 +1,42 @@
+package deploy
+
+import (
+	"io"
+	"sync"
+
+	"github.com/luizalabs/teresa/pkg/server/app"
+	"github.com/luizalabs/teresa/pkg/server/auth"
+	"github.com/luizalabs/teresa/pkg/server/database"
+)
+
+type FakeOperations struct {
+	mutex   *sync.RWMutex
+	Storage map[string]bool
+}
+
+func hasPerm(email string) bool {
+	return email != "bad-user@luizalabs.com"
+}
+
+func (f *FakeOperations) List(user *database.User, appName string) ([]*ReplicaSetListItem, error) {
+	f.mutex.RLock()
+	defer f.mutex.RUnlock()
+
+	if !hasPerm(user.Email) {
+		return nil, auth.ErrPermissionDenied
+	}
+
+	if _, found := f.Storage[appName]; !found {
+		return nil, app.ErrNotFound
+	}
+
+	return []*ReplicaSetListItem{}, nil
+}
+
+func (f *FakeOperations) Deploy(user *database.User, appName string, tarBall io.ReadSeeker, description string, opts *Options) (io.ReadCloser, error) {
+	return nil, nil
+}
+
+func NewFakeOperations() Operations {
+	return &FakeOperations{mutex: &sync.RWMutex{}, Storage: make(map[string]bool)}
+}

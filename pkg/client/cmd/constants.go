b844963ecc397eac51f12d8b039526c469ffcfb7
Arnaldo de Moraes Pereira <arnaldo.pereira@luizalabs.com>
2017-10-10 11:56:27 -0300

    remove unused vars

--- pkg/client/cmd/constants.go
+++ pkg/client/cmd/constants.go
@@ -2,19 +2,7 @@ package cmd
 
 // variables used to capture the cli flags
 var (
-	cfgFile         string
-	cfgCluster      string
-	debugFlag       bool
-	teamIDFlag      int64
-	teamNameFlag    string
-	teamEmailFlag   string
-	teamURLFlag     string
-	appNameFlag     string
-	appScaleFlag    int
-	descriptionFlag string
-)
-
-const (
-	deploymentSuccessMark = "----------deployment-success----------"
-	deploymentErrorMark   = "----------deployment-error----------"
+	cfgFile    string
+	cfgCluster string
+	debugFlag  bool
 )

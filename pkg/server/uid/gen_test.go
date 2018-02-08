a4d31d0803278d98899d18952b831a13bf9de265
Diego Garcia <drgarcia1986@gmail.com>
2018-02-08 17:26:56 -0200

    Add package `uid` to generalize pods id generation

---
+++ pkg/server/uid/gen_test.go
@@ -0,0 +1,14 @@
+package uid
+
+import "testing"
+
+func TestGen(t *testing.T) {
+	generatedIds := make(map[string]bool)
+	for i := 0; i < 10; i++ {
+		gId := New()
+		if _, found := generatedIds[gId]; found {
+			t.Fatal("collision detected")
+			generatedIds[gId] = true
+		}
+	}
+}

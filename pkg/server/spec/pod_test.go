a47efe8e56a37d610701872747cb5f1dd99d4c3a
Diego Garcia <drgarcia1986@gmail.com>
2018-07-26 15:37:20 -0300

    Add methods to create volumes based on secrets keys on spec pkg

--- pkg/server/spec/pod_test.go
+++ pkg/server/spec/pod_test.go
@@ -22,9 +22,10 @@ func TestPodBuilder(t *testing.T) {
 	ng := NewNginxContainer("image:v2", &app.App{})
 	svo := ShareVolumeBetweenAppAndSideCar("vol", "/app")
 	mcm := MountConfigMapInSideCar("cm", "/cm", "cm")
+	msia := MountSecretItemsInAppContainer("secret", "/s", "secret", []string{"s", "e"})
 
 	ps := NewPodBuilder(expectedName, expectedNamespace).
-		WithAppContainer(cn).
+		WithAppContainer(cn, msia).
 		WithInitContainer(init, mso, svio).
 		WithSideCar(ng, svo, mcm, SwitchPortWithAppContainer).
 		Build()
@@ -47,11 +48,24 @@ func TestPodBuilder(t *testing.T) {
 	if actual := len(ps.InitContainers); actual != 1 {
 		t.Errorf("expected 1, got %d", actual)
 	}
-	// init secret, init shared with app, nginx config map, nginx shared with app
-	if actual := len(ps.Volumes); actual != 4 {
-		t.Errorf("expected 4, got %d", actual)
+	// init secret, init shared with app, nginx config map, nginx shared with app, secrets app
+	if actual := len(ps.Volumes); actual != 5 {
+		t.Errorf("expected 5, got %d", actual)
 	}
-	if actual := len(ps.Containers[0].VolumeMounts); actual != 2 {
+	if actual := len(ps.Containers[0].VolumeMounts); actual != 3 {
 		t.Errorf("expected 2, got %d", actual)
+	}
+
+	found := false
+	for _, vol := range ps.Volumes {
+		if vol.Name == "secret" {
+			if len(vol.Items) != 2 {
+				t.Errorf("expected 2, got %d", len(vol.Items))
+			}
+			found = true
+		}
+	}
+	if !found {
+		t.Error("volume with secrets not found")
 	}
 }

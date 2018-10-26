697fe5434d56d3434295ca21272b6e29d0c4c0dd
Vinicyus Macedo <7549205+vinicyusmacedo@users.noreply.github.com>
2018-10-26 17:51:53 -0300

    Removed env vars for instance connection string

--- pkg/server/spec/cloudsqlproxy_test.go
+++ pkg/server/spec/cloudsqlproxy_test.go
@@ -26,57 +26,7 @@ func TestNewCloudSQLProxy(t *testing.T) {
 		},
 	}
 
-	csp, err := NewCloudSQLProxy(img, ty, &app.App{})
-	if err != nil {
-		t.Fatal("got unexpected error:", err)
-	}
-	if !reflect.DeepEqual(csp, want) {
-		t.Errorf("got %v; want %v", csp, want)
-	}
-}
-
-func TestNewCloudSQLProxyFromEnvVar(t *testing.T) {
-	a := &app.App{
-		EnvVars: []*app.EnvVar{
-			&app.EnvVar{Key: "DB_PROJECT", Value: "project"},
-			&app.EnvVar{Key: "DB_ZONE", Value: "zone"},
-			&app.EnvVar{Key: "DB_NAME", Value: "name"},
-		},
-	}
-	img := "image"
-
-	want := &CloudSQLProxy{
-		Instances:      "$(DB_PROJECT):$(DB_ZONE):$(DB_NAME)=tcp:3306",
-		CredentialFile: "file",
-		Image:          img,
-	}
-	fn := func(v interface{}) error {
-		v.(*CloudSQLProxy).CredentialFile = want.CredentialFile
-		return nil
-	}
-	ty := &TeresaYaml{
-		SideCars: map[string]RawData{
-			"cloudsql-proxy": {fn},
-		},
-	}
-
-	csp, err := NewCloudSQLProxy(img, ty, a)
-	if err != nil {
-		t.Fatal("got unexpected error:", err)
-	}
-	if !reflect.DeepEqual(csp, want) {
-		t.Errorf("got %v; want %v", csp, want)
-	}
-	a = &app.App{
-		EnvVars: []*app.EnvVar{
-			&app.EnvVar{Key: "GCP_CLOUDSQL_INSTANCE_NAME", Value: "project:zone:name=tcp:3306"},
-			&app.EnvVar{Key: "DB_PROJECT", Value: "a_project"},
-			&app.EnvVar{Key: "DB_ZONE", Value: "a_zone"},
-			&app.EnvVar{Key: "DB_NAME", Value: "a_name"},
-		},
-	}
-	want.Instances = "project:zone:name=tcp:3306"
-	csp, err = NewCloudSQLProxy(img, ty, a)
+	csp, err := NewCloudSQLProxy(img, ty)
 	if err != nil {
 		t.Fatal("got unexpected error:", err)
 	}
@@ -95,7 +45,7 @@ func TestNewCloudSQLProxyError(t *testing.T) {
 		},
 	}
 
-	if _, err := NewCloudSQLProxy("test", ty, &app.App{}); err == nil {
+	if _, err := NewCloudSQLProxy("test", ty); err == nil {
 		t.Error("got nil; want error")
 	}
 }
@@ -107,60 +57,12 @@ func TestNewCloudSQLProxyNil(t *testing.T) {
 		},
 	}
 
-	csp, err := NewCloudSQLProxy("test", ty, &app.App{})
+	csp, err := NewCloudSQLProxy("test", ty)
 	if err != nil {
 		t.Fatal("got unexpected error:", err)
 	}
 	if csp != nil {
 		t.Errorf("got %v; want nil", csp)
-	}
-}
-
-func TestNewCloudSQLProxyContainerFromEnvVar(t *testing.T) {
-	a := &app.App{
-		EnvVars: []*app.EnvVar{
-			&app.EnvVar{Key: "DB_PROJECT", Value: "project"},
-			&app.EnvVar{Key: "DB_ZONE", Value: "zone"},
-			&app.EnvVar{Key: "DB_NAME", Value: "name"},
-		},
-	}
-	csp := &CloudSQLProxy{
-		Instances:      "$(DB_PROJECT):$(DB_ZONE):$(DB_NAME)=tcp:3306",
-		CredentialFile: "file",
-		Image:          "image",
-	}
-	want := &Container{
-		Name: "cloudsql-proxy",
-		ContainerLimits: &ContainerLimits{
-			CPU:    cloudSQLProxyDefaultCPULimit,
-			Memory: cloudSQLProxyDefaultMemoryLimit,
-		},
-		Image:   csp.Image,
-		Command: []string{"/cloud_sql_proxy"},
-		Args: []string{
-			"-instances=$(DB_PROJECT):$(DB_ZONE):$(DB_NAME)=tcp:3306",
-			"-credential_file=/secrets/cloudsql/file",
-		},
-		VolumeMounts: []*VolumeMounts{
-			{
-				Name:      AppSecretName,
-				MountPath: "/secrets/cloudsql/file",
-				SubPath:   "file",
-				ReadOnly:  true,
-			},
-		},
-		Env: map[string]string{
-			"DB_PROJECT": "project",
-			"DB_ZONE":    "zone",
-			"DB_NAME":    "name",
-		},
-		Ports:   []Port{},
-		Secrets: []string{},
-	}
-
-	cn := NewCloudSQLProxyContainer(csp, a)
-	if !reflect.DeepEqual(cn, want) {
-		t.Errorf("got %v; want %v", cn, want)
 	}
 }
 
